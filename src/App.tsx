5cd424033feba5d41ccb96b59dae3d423c291b40
Abhinav2091 <abhinavsharma043@gmail.com>
2023-11-03 02:34:39 +0530

    2 component Explore top books.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExloreTopBooks } from './layouts/Homepage/ExploreTopBooks';
 
 
 function App() {
   return (
+    <div>
     <Navbar/>
+    <ExloreTopBooks/>
+    </div>
   );
 }
 
