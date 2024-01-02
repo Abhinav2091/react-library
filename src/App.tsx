fdae78acd8bfaa8fb7a5df9c682d97f0eec493a4
Abhinav Sharma <41360473+Abhinav2091@users.noreply.github.com>
2024-01-03 05:09:59 +0530

    moving forward with react learning

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/Homepage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { BookCheckoutPage } from './layouts/BookCheckoutpage/BookCheckoutPage';
 
 
 export const App = () => {
@@ -21,6 +22,9 @@ export const App = () => {
           </Route>
           <Route path='/search'>
             <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage />
           </Route>
         </Switch>
       </div>
